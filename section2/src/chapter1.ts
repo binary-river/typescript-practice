ca7c9286946846e246d0a8e172cc263ff555c8b9
binary-river <happychrome@naver.com>
2024-01-07 08:57:11 +0900

    section2 update

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,21 @@
+
+//number
+let num1:number = 123;
+let num2:number = NaN;
+
+//string
+let str1:string = "abc";
+let str2:string = `aa ${str1}`;
+
+//boolean
+let bool1:boolean = true;
+
+//null
+let null1:null = null;
+
+//undefined
+let unde1:undefined = undefined;
+
+//literal
+let lit1: 10 = 10;
+let lit2: "abc" = "abc";
---
+++ section2/src/chapter2.ts
@@ -0,0 +1,10 @@
+
+//number array 
+let numArr: number[] = [1,2,3,4];
+
+//string array
+let strArr: string[] = ["apple", "hello", "world"];
+
+//generic type
+let numArr2: Array<number> = [1,2,3,4];
+
