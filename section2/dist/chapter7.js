2ee61c6bba8fb2320a1b399335a0ad1c42cdde40
binary-river <happychrome@naver.com>
2024-01-08 23:44:37 +0900

    chapter7 update

---
+++ section2/dist/chapter7.js
@@ -0,0 +1,11 @@
+//void
+function func2() {
+    console.log("hello void");
+}
+;
+let a = undefined;
+//never
+function func1() {
+    while (true) { }
+}
+export {};
---
+++ section2/src/chapter7.ts
@@ -0,0 +1,11 @@
+//void
+function func2(): void {
+    console.log("hello void");
+};
+
+let a:void = undefined;
+
+//never
+function func1(): never {
+    while(true) {}
+}
